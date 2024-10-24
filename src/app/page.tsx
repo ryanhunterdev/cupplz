5513c43df8c8a84c15c83d6a2e147042b5f4e0de
Ryan Hunter <ryanhunterdev@gmail.com>
2024-10-25 08:19:09 +1100

    add to home screen prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import CoffeeCalculator from "@/components/CoffeeCalculator/CoffeeCalculator";
 
 import { useEffect, useState } from "react";
 
+import Share from '@/icons/share'
+
 export default function Home() {
     const [showIntro, setShowIntro] = useState(true);
 
@@ -20,6 +22,10 @@ export default function Home() {
         <div className={s.Home}>
             <h1>Cupplz</h1>
             <CoffeeCalculator />
+            <div className={s.AddApp}>
+                <p>download app!</p>
+                <p>Press <span>{Share}</span> and "Add to Home Screen"</p>
+            </div>
             <div className={cn(s.Intro, showIntro && s.Show)}>
                 <img src="/logo.svg" alt="Cupplz" />
             </div>
